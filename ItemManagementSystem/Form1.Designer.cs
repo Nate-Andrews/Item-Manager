bda12cadf9506a7cec90f7d7631e6be66ca3a74e
Nate-Andrews <n.andrew244@gmail.com>
2022-09-29 19:37:06 -0400

    Commit 1

---
+++ ItemManagementSystem/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿
+namespace ItemManagementSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label itemIdLabel;
+            System.Windows.Forms.Label nameLabel;
+            System.Windows.Forms.Label priceLabel;
+            System.Windows.Forms.Label departmentLabel;
+            this.database1DataSet = new ItemManagementSystem.Database1DataSet();
+            this.itemsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.itemsTableAdapter = new ItemManagementSystem.Database1DataSetTableAdapters.ItemsTableAdapter();
+            this.tableAdapterManager = new ItemManagementSystem.Database1DataSetTableAdapters.TableAdapterManager();
+            this.itemIdTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.priceTextBox = new System.Windows.Forms.TextBox();
+            this.departmentTextBox = new System.Windows.Forms.TextBox();
+            this.itemsDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.buttonf = new System.Windows.Forms.Button();
+            this.buttonc = new System.Windows.Forms.Button();
+            this.buttonu = new System.Windows.Forms.Button();
+            this.buttond = new System.Windows.Forms.Button();
+            this.buttonr = new System.Windows.Forms.Button();
+            itemIdLabel = new System.Windows.Forms.Label();
+            nameLabel = new System.Windows.Forms.Label();
+            priceLabel = new System.Windows.Forms.Label();
+            departmentLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.itemsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.itemsDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // itemIdLabel
+            // 
+            itemIdLabel.AutoSize = true;
+            itemIdLabel.Location = new System.Drawing.Point(13, 13);
+            itemIdLabel.Name = "itemIdLabel";
+            itemIdLabel.Size = new System.Drawing.Size(42, 13);
+            itemIdLabel.TabIndex = 1;
+            itemIdLabel.Text = "Item Id:";
+            // 
+            // nameLabel
+            // 
+            nameLabel.AutoSize = true;
+            nameLabel.Location = new System.Drawing.Point(13, 39);
+            nameLabel.Name = "nameLabel";
+            nameLabel.Size = new System.Drawing.Size(38, 13);
+            nameLabel.TabIndex = 3;
+            nameLabel.Text = "Name:";
+            // 
+            // priceLabel
+            // 
+            priceLabel.AutoSize = true;
+            priceLabel.Location = new System.Drawing.Point(13, 65);
+            priceLabel.Name = "priceLabel";
+            priceLabel.Size = new System.Drawing.Size(34, 13);
+            priceLabel.TabIndex = 5;
+            priceLabel.Text = "Price:";
+            // 
+            // departmentLabel
+            // 
+            departmentLabel.AutoSize = true;
+            departmentLabel.Location = new System.Drawing.Point(13, 91);
+            departmentLabel.Name = "departmentLabel";
+            departmentLabel.Size = new System.Drawing.Size(65, 13);
+            departmentLabel.TabIndex = 7;
+            departmentLabel.Text = "Department:";
+            // 
+            // database1DataSet
+            // 
+            this.database1DataSet.DataSetName = "Database1DataSet";
+            this.database1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // itemsBindingSource
+            // 
+            this.itemsBindingSource.DataMember = "Items";
+            this.itemsBindingSource.DataSource = this.database1DataSet;
+            // 
+            // itemsTableAdapter
+            // 
+            this.itemsTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.ItemsTableAdapter = this.itemsTableAdapter;
+            this.tableAdapterManager.UpdateOrder = ItemManagementSystem.Database1DataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // itemIdTextBox
+            // 
+            this.itemIdTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.itemsBindingSource, "itemId", true));
+            this.itemIdTextBox.Location = new System.Drawing.Point(84, 10);
+            this.itemIdTextBox.Name = "itemIdTextBox";
+            this.itemIdTextBox.Size = new System.Drawing.Size(100, 20);
+            this.itemIdTextBox.TabIndex = 2;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.itemsBindingSource, "name", true));
+            this.nameTextBox.Location = new System.Drawing.Point(84, 36);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nameTextBox.TabIndex = 4;
+            // 
+            // priceTextBox
+            // 
+            this.priceTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.itemsBindingSource, "price", true));
+            this.priceTextBox.Location = new System.Drawing.Point(84, 62);
+            this.priceTextBox.Name = "priceTextBox";
+            this.priceTextBox.Size = new System.Drawing.Size(100, 20);
+            this.priceTextBox.TabIndex = 6;
+            // 
+            // departmentTextBox
+            // 
+            this.departmentTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.itemsBindingSource, "Department", true));
+            this.departmentTextBox.Location = new System.Drawing.Point(84, 88);
+            this.departmentTextBox.Name = "departmentTextBox";
+            this.departmentTextBox.Size = new System.Drawing.Size(100, 20);
+            this.departmentTextBox.TabIndex = 8;
+            // 
+            // itemsDataGridView
+            // 
+            this.itemsDataGridView.AutoGenerateColumns = false;
+            this.itemsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.itemsDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4});
+            this.itemsDataGridView.DataSource = this.itemsBindingSource;
+            this.itemsDataGridView.Location = new System.Drawing.Point(16, 137);
+            this.itemsDataGridView.Name = "itemsDataGridView";
+            this.itemsDataGridView.Size = new System.Drawing.Size(444, 220);
+            this.itemsDataGridView.TabIndex = 8;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "itemId";
+            this.dataGridViewTextBoxColumn1.HeaderText = "itemId";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "name";
+            this.dataGridViewTextBoxColumn2.HeaderText = "name";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "price";
+            this.dataGridViewTextBoxColumn3.HeaderText = "price";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "Department";
+            this.dataGridViewTextBoxColumn4.HeaderText = "Department";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            // 
+            // buttonf
+            // 
+            this.buttonf.Location = new System.Drawing.Point(201, 10);
+            this.buttonf.Name = "buttonf";
+            this.buttonf.Size = new System.Drawing.Size(75, 23);
+            this.buttonf.TabIndex = 9;
+            this.buttonf.Text = "Find";
+            this.buttonf.UseVisualStyleBackColor = true;
+            this.buttonf.Click += new System.EventHandler(this.buttonf_Click);
+            // 
+            // buttonc
+            // 
+            this.buttonc.Location = new System.Drawing.Point(201, 88);
+            this.buttonc.Name = "buttonc";
+            this.buttonc.Size = new System.Drawing.Size(75, 23);
+            this.buttonc.TabIndex = 10;
+            this.buttonc.Text = "Create";
+            this.buttonc.UseVisualStyleBackColor = true;
+            this.buttonc.Click += new System.EventHandler(this.buttonc_Click);
+            // 
+            // buttonu
+            // 
+            this.buttonu.Location = new System.Drawing.Point(292, 88);
+            this.buttonu.Name = "buttonu";
+            this.buttonu.Size = new System.Drawing.Size(75, 23);
+            this.buttonu.TabIndex = 11;
+            this.buttonu.Text = "Update";
+            this.buttonu.UseVisualStyleBackColor = true;
+            this.buttonu.Click += new System.EventHandler(this.buttonu_Click);
+            // 
+            // buttond
+            // 
+            this.buttond.Location = new System.Drawing.Point(386, 88);
+            this.buttond.Name = "buttond";
+            this.buttond.Size = new System.Drawing.Size(75, 23);
+            this.buttond.TabIndex = 12;
+            this.buttond.Text = "Delete";
+            this.buttond.UseVisualStyleBackColor = true;
+            this.buttond.Click += new System.EventHandler(this.buttond_Click);
+            // 
+            // buttonr
+            // 
+            this.buttonr.Location = new System.Drawing.Point(201, 363);
+            this.buttonr.Name = "buttonr";
+            this.buttonr.Size = new System.Drawing.Size(75, 23);
+            this.buttonr.TabIndex = 13;
+            this.buttonr.Text = "Reset";
+            this.buttonr.UseVisualStyleBackColor = true;
+            this.buttonr.Click += new System.EventHandler(this.buttonr_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 398);
+            this.Controls.Add(this.buttonr);
+            this.Controls.Add(this.buttond);
+            this.Controls.Add(this.buttonu);
+            this.Controls.Add(this.buttonc);
+            this.Controls.Add(this.buttonf);
+            this.Controls.Add(this.itemsDataGridView);
+            this.Controls.Add(itemIdLabel);
+            this.Controls.Add(this.itemIdTextBox);
+            this.Controls.Add(nameLabel);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(priceLabel);
+            this.Controls.Add(this.priceTextBox);
+            this.Controls.Add(departmentLabel);
+            this.Controls.Add(this.departmentTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.itemsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.itemsDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Database1DataSet database1DataSet;
+        private System.Windows.Forms.BindingSource itemsBindingSource;
+        private Database1DataSetTableAdapters.ItemsTableAdapter itemsTableAdapter;
+        private Database1DataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.TextBox itemIdTextBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox priceTextBox;
+        private System.Windows.Forms.TextBox departmentTextBox;
+        private System.Windows.Forms.DataGridView itemsDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.Button buttonf;
+        private System.Windows.Forms.Button buttonc;
+        private System.Windows.Forms.Button buttonu;
+        private System.Windows.Forms.Button buttond;
+        private System.Windows.Forms.Button buttonr;
+    }
+}
+
---
+++ ItemManagementSystem/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ItemManagementSystem
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection("Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename = C:\\Users\\andre\\Documents\\Coding\\C#\\ItemManagementSystem\\ItemManagementSystem\\Database1.mdf;Integrated Security=True");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void itemsBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.itemsBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.database1DataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'database1DataSet.Items' table. You can move, or remove it, as needed.
+            this.itemsTableAdapter.Fill(this.database1DataSet.Items);
+
+            clear();
+            
+            con.Open();
+            SqlCommand command = new SqlCommand("SELECT * FROM Items", con);
+            SqlDataAdapter da = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            itemsDataGridView.DataSource = dt;
+            con.Close();
+        }
+
+        private void buttonc_Click(object sender, EventArgs e)
+        { 
+            con.Open();
+            SqlCommand command = new SqlCommand("Insert INTO Items(itemId, name, price, Department) VALUES (@itemId, @name, @price, @Department)", con);
+            command.Parameters.AddWithValue("@itemID", itemIdTextBox.Text);
+            command.Parameters.AddWithValue("@name", nameTextBox.Text);
+            command.Parameters.AddWithValue("@price", priceTextBox.Text);
+            command.Parameters.AddWithValue("@Department", departmentTextBox.Text);
+            command.ExecuteNonQuery();
+            con.Close();
+
+            clear();
+
+            MessageBox.Show("Created Item");
+        }
+        
+        private void buttonu_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand command = new SqlCommand("UPDATE Items SET name=@name, price=@price, Department=@Department WHERE itemId=@itemId", con);
+            command.Parameters.AddWithValue("@itemID", itemIdTextBox.Text);
+            command.Parameters.AddWithValue("@name", nameTextBox.Text);
+            command.Parameters.AddWithValue("@price", priceTextBox.Text);
+            command.Parameters.AddWithValue("@Department", departmentTextBox.Text);
+            command.ExecuteNonQuery();
+            con.Close();
+
+            clear();
+
+            MessageBox.Show("Item Updated");
+        }
+
+        private void buttond_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand command = new SqlCommand("Delete Items WHERE itemId=@itemId", con);
+            command.Parameters.AddWithValue("@itemID", itemIdTextBox.Text);
+            command.ExecuteNonQuery();
+            con.Close();
+
+            clear();
+
+            MessageBox.Show("Item Deleted");
+        }
+
+        private void buttonf_Click(object sender, EventArgs e)
+        { 
+            con.Open();
+            SqlCommand command = new SqlCommand("SELECT * FROM Items WHERE itemId=@itemId", con);
+            command.Parameters.AddWithValue("@itemID", itemIdTextBox.Text);
+            SqlDataAdapter da = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            itemsDataGridView.DataSource = dt;
+            con.Close();
+
+            clear();
+        }
+
+        private void buttonr_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand command = new SqlCommand("SELECT * FROM Items", con);
+            SqlDataAdapter da = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            itemsDataGridView.DataSource = dt;
+            con.Close();
+        }
+
+        void clear()
+        {
+            itemIdTextBox.Text = "";
+            nameTextBox.Text = "";
+            priceTextBox.Text = "";
+            departmentTextBox.Text = "";
+        }
+    }
+}
